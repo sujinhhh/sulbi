9714bf25b4d4e3d1d79de05c206035ea59b43864
Sujin Lee <ccclalasj@gmail.com>
2021-06-18 18:02:22 -0400

    hero

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,23 @@
 const btnHamberger = document.querySelector("#btnHamberger");
+const body = document.querySelector("body");
 const header = document.querySelector(".header");
+// const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
 
 btnHamberger.addEventListener("click", () => {
   if (header.classList.contains("open")) {
     header.classList.remove("open");
+    body.classList.remove("noscroll");
+    fadeElems.forEach((element) => {
+      element.classList.add("fade-out");
+      element.classList.remove("fade-in");
+    });
   } else {
     header.classList.add("open");
+    body.classList.add("noscroll");
+    fadeElems.forEach((element) => {
+      element.classList.add("fade-in");
+      element.classList.remove("fade-out");
+    });
   }
 });
