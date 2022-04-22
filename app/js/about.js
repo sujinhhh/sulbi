113a1699cb273867b342f691f3bf07297583b099
Sujin Lee <ccclalasj@gmail.com>
2022-04-21 21:30:14 -0400

    remove modules

---
+++ app/js/about.js
@@ -0,0 +1,13 @@
+let countEl = document.getElementById('count-el')
+let count = 0
+function increment(){
+  count = count + 1
+  countEl.innerText = count  
+}
+
+increment()
+increment()
+increment()
+
+let i = "sujin"
+console.log(typeof i);
