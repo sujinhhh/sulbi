d80bdf87074a562c58e44d802547055507cf03fe
Sujin Lee <ccclalasj@gmail.com>
2022-04-23 10:26:50 -0400

    test

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,6 @@
-let countEl = document.getElementById('count-el')
-let count = 0
-function increment(){
-  count = count + 1
-  countEl.innerText = count  
+let countEl = document.getElementById("count-el");
+let count = 0;
+function increment() {
+  count = count + 1;
+  countEl.innerText = count;
 }
-
-increment()
-increment()
-increment()
-
-let i = "sujin"
-console.log(typeof i   );
